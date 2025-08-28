03d3324487f47c518d05e39e9d8872ca7477affd
Kirill Ivanov <kirillunlimited@gmail.com>
2025-08-28 20:09:06 +0200

    One page update

--- _data/sites/kirillunlimited.js
+++ _data/sites/kirillunlimited.js
@@ -1,16 +1,11 @@
 module.exports = {
-	name: "kirillunlimited.com", // optional, falls back to object key
-	description: "Kirill's Personal web site",
-	skip: false,
-	options: {
-		runs: 1,
-		frequency: 60 * 24, // 24 hours
-		freshChrome: "site"
-	},
-	urls: [
-		"https://kirillunlimited.com/",
-		"https://kirillunlimited.com/about/",
-		"https://kirillunlimited.com/contact/",
-		"https://kirillunlimited.com/blog/",
-	]
+  name: "kirillunlimited.com", // optional, falls back to object key
+  description: "Kirill's Personal web site",
+  skip: false,
+  options: {
+    runs: 1,
+    frequency: 60 * 24, // 24 hours
+    freshChrome: "site",
+  },
+  urls: ["https://kirillunlimited.com/"],
 };
